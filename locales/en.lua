db3ed1b55e00f02b8f02d609a1ce801f9c4b2079
Bro0kS <60176872+bro0ks12@users.noreply.github.com>
2023-06-06 11:38:23 +0100

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,104 @@
+Locales['en'] = {
+  -- Cloackroom
+  ['cloakroom'] = 'Wardrobe',
+  ['citizen_wear'] = 'Plain clothes',
+  ['police_wear'] = 'Work wear',
+  ['open_cloackroom'] = 'Press ~INPUT_CONTEXT~ to change',
+  -- Armory
+  ['get_weapon'] = 'Get weapons',
+  ['put_weapon'] = 'Bring weapons',
+  ['buy_weapons'] = 'Buy weapons',
+  ['armory'] = 'Armory',
+  ['open_armory'] = 'Press ~INPUT_CONTEXT~ to open the armory',
+  -- Vehicles
+  ['vehicle_menu'] = 'Vehicle',
+  ['vehicle_out'] = 'There is already a vehicle outside',
+  ['vehicle_spawner'] = 'Press ~INPUT_CONTEXT~ to get a vehicle',
+  ['store_vehicle'] = 'Press ~INPUT_CONTEXT~ to park the vehicle',
+  ['service_max'] = 'Max police officers on duty:',
+  -- Action Menu
+
+  ['citizen_interaction'] = 'civilian actions',
+  ['vehicle_interaction'] = 'vehicle interactions',
+  ['object_spawner'] = 'Object Spawner',
+
+  
+  ['id_card'] = 'ID card',
+  ['search'] = 'search',
+  ['handcuff'] = 'arrest / release',
+  ['put_in_vehicle'] = 'Put in vehicle',
+  ['fine'] = 'penalty',
+  ['no_players_nearby'] = 'No players nearby',
+
+  
+  ['vehicle_info'] = 'vehicle info',
+  ['pick_lock'] = 'Open vehicle',
+  ['vehicle_unlocked'] = 'vehicle ~g~open~s~',
+  ['no_vehicles_nearby'] = 'No vehicles nearby',
+  ['traffic_interaction'] = 'Street Interactions',
+  ['cone'] = 'hat',
+  ['barrier'] = 'barrier',
+  ['spikestrips'] = 'nail tape',
+  ['box'] = 'box',
+  ['cash'] = 'box with money',
+  -- ID Card Menu
+
+  ['name'] = 'name : ',
+  ['bac'] = 'bAC : ',
+
+  -- Body Search Menu
+
+  ['confiscate_dirty'] = 'Confiscated black money: $',
+  ['guns_label'] = '--- guns ---',
+  ['confiscate'] = 'confiscate',
+  ['inventory_label'] = '--- inventory ---',
+  ['confiscate_inv'] = 'confect x',
+
+  
+  ['traffic_offense'] = 'Traffic is passing',
+  ['minor_offense'] = 'Minor offense',
+  ['average_offense'] = 'normal offense',
+  ['major_offense'] = 'High Pass',
+  ['fine_total'] = 'Penalty:',
+  -- Vehicle Info Menu
+  
+  ['plate'] = 'Plate:',
+  ['owner_unknown'] = 'Owner: Unknown',
+  ['owner'] = 'owner:',
+  -- Weapons Menus
+  
+  ['get_weapon_menu'] = 'Armory - take weapons',
+  ['put_weapon_menu'] = 'Armory - Bring Arms',
+  ['buy_weapon_menu'] = 'Armory - buy weapons',
+  ['not_enough_money'] = 'You dont have enough money',
+  -- Boss Menu
+  
+  ['take_company_money'] = 'Withdraw corporate money',
+  ['deposit_money'] = 'Deposit money',
+  ['amount_of_withdrawal'] = 'Amount to withdraw',
+  ['invalid_amount'] = 'invalid amount',
+  ['amount_of_deposit'] = 'Amount to deposit',
+  ['open_bossmenu'] = 'Press ~INPUT_CONTEXT~ to open the menu',
+  -- Misc
+  
+  ['remove_object'] = 'Press ~ INPUT_CONTEXT ~ to remove the object',
+  ['map_blip'] = 'TheLostMC',
+  -- Notifications
+  
+  ['from'] = '~s~ From ~b~',
+  ['you_have_confinv'] = 'you confiscated ~y~x',
+  ['confinv'] = '~s~ confiscated ~y~x from you',
+  ['you_have_confdm'] = 'you confiscated ~y~$',
+  ['confdm'] = '~s~ confiscated ~y~$ from you',
+  ['you_have_confweapon'] = 'you confiscated ~y~x1',
+  ['confweapon'] = '~s~ confiscated ~y~x1 from you',
+  ['alert_police'] = 'Alert police',
+  -- Authorized Vehicles
+ 
+  ['police'] = 'patrol vehicle 1',
+  ['police2'] = 'patrol vehicle 2',
+  ['police3'] = 'patrol vehicle 3',
+  ['police4'] = 'civil vehicle',
+  ['policeb'] = 'motorcycle',
+  ['policet'] = 'Transporter',
+}
