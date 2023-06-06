d8257a30f0e939b33e1878694ba60734758d49f1
bro0ks12 <mrnutlol7777@gmail.com>
2023-06-06 11:59:42 +0100

    Added CheckVersion

--- server/server.lua
+++ server/server.lua
@@ -143,3 +143,24 @@ AddEventHandler('onServerResourceStart', function(resourceName)
       exports.ox_inventory:RegisterStash(stash.id, stash.label, stash.slots, stash.weight, stash.groups)
   end
 end)
+
+Citizen.CreateThread( function()
+  updatePath = "/bro0ks12/esx_AdvancedMafia/releases" -- your git user/repo path
+  resourceName = "esx_AdvancedMafia ("..GetCurrentResourceName()..")" -- the resource name
+  
+  function checkVersion(err,responseText, headers)
+    curVersion = LoadResourceFile(GetCurrentResourceName(), "version") -- make sure the "version" file actually exists in your resource root!
+  
+    if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
+      print("\n###############################")
+      print("\n"..resourceName.." is outdated, should be:\n"..responseText.."is:\n"..curVersion.."\nplease update it from https://github.com"..updatePath.."")
+      print("\n###############################")
+    elseif tonumber(curVersion) > tonumber(responseText) then
+      print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online i advise you to update ( or downgrade? )")
+    else
+      print("\n"..resourceName.." is up to date, have fun!")
+    end
+  end
+  
+  PerformHttpRequest("https://github.com/"..updatePath, checkVersion, "GET")
+  end)
